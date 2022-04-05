825be2bcc73783e145434e13a5444a0e640e0be3
salih <salihpoozhitharaa@gmail.com>
2022-04-05 16:18:19 +0530

    one way binding using template variable

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
   
   //database
 
-  database = {
+  database:any = {
     1000:{acno:1000,uname:"salih",password:1000,balance:5000},
     1001:{acno:1000,uname:"ijilan",password:1001,balance:8000},
     1002:{acno:1000,uname:"aarish",password:1002,balance:10000}
@@ -30,13 +30,55 @@ export class LoginComponent implements OnInit {
     // console.log(this.acno);
     
   }
-  passChange(event:any){
+  pswdChange(event:any){
     this.pswd=event.target.value
     // console.log(this.pswd);
   }
 
-  login(){
-    alert("login success!!")
+  // login(){
+
+  //   var acno=this.acno
+  //   var pswd=this.pswd
+
+  //   let database=this.database
+
+  //   if(acno in database){
+  //     if(pswd == database[acno]["password"]){
+  //       alert("login success!!")
+
+  //     }else{
+  //       alert("invalid password")
+  //     }
+  //   }else{
+  //     alert("user does not exist")
+  //   }
+
+  // }
+
+  // login using template ref
+
+  login(a:any,p:any){
+
+    console.log(a.value);
+    
+
+    //user entered acno nd passwd
+    var acno=a.value
+    var pswd=p.value
+
+    let database=this.database
+
+    if(acno in database){
+      if(pswd == database[acno]["password"]){
+        alert("login success!!")
+
+      }else{
+        alert("invalid password")
+      }
+    }else{
+      alert("user does not exist")
+    }
+
   }
 
 
